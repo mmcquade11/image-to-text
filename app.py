8045f9ceaa42299373d45266bb77a33b191c0108
Mark McQuade <mark@newagetechie.com>
2021-11-18 14:13:51 -0500

    added github link

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("Easy OCR - Extract Text from Images")
 #subtitle
 st.markdown("## Optical Character Recognition - Using `easyocr`, `streamlit` -  hosted on 🤗 Spaces")
 
-st.markdown("Link to the app - [image-to-text-app on 🤗 Spaces](https://huggingface.co/spaces/Amrrs/image-to-text-app)")
+st.markdown("## Link to GitHub - [image-to-text-app](https://github.com/mmcquade11/image-to-text)")
 
 #image uploader
 image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
@@ -44,4 +44,4 @@ if image is not None:
 else:
     st.write("Upload an Image")
 
-st.caption("Made with ❤️ by @1littlecoder. Credits to 🤗 Spaces for Hosting this ")
+# .caption("Made with ❤️ by @1littlecoder. Credits to 🤗 Spaces for Hosting this ")
